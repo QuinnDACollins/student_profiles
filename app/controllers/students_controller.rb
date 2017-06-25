a260500c45f3840616f849809d3f573b4913a1d2
Quinn Collins <greendabean@gmail.com>
2017-06-25 20:00:50 +0000

    Added Bio, Restaurant, TV, and Name

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,3 +1,10 @@
 class StudentsController < ApplicationController
 
+def Quinn
+    @restaurant = "Strings"
+    @name = "Quinn Collins"
+    @tv = "The Office"
+    @bio = "Grew up in Chicago, Illinois. Born in 2001, owned a single cat. Once that cat passed I got a new cat. I strive to have a job relating to programming/IT."
+    
+end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+    get '/Quinn', to: 'students#Quinn'
 # Follow this format for your route: get '/harsha', to: 'students#harsha'
 end
