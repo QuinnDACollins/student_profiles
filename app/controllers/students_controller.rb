c8421d4915149b9c2b08165727ec0426d7a44a62
Quinn Collins <greendabean@gmail.com>
2017-06-25 20:04:27 +0000

    Added Bio, Restaurant, TV, and Name

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,7 +4,7 @@ def Quinn
     @restaurant = "Strings"
     @name = "Quinn Collins"
     @tv = "The Office"
-    @bio = "Grew up in Chicago, Illinois. Born in 2001, owned a single cat. Once that cat passed I got a new cat. I strive to have a job relating to programming/IT."
+    @bio = "Grew up in Chicago, Illinois. Born in 2001, owned a singlular cat. Once that cat passed I got a new cat. I strive to have a job relating to programming/IT."
     
 end 
 end
